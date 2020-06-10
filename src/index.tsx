844edf95aa51f08dda8409646fd34716f9246e5c
Alistair Smith <alistairsmith01@gmail.com>
2020-06-10 23:33:23 +0100

    BLM

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Store from './core/store';
+// import Store from './core/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Store.Container>
-      <App />
-    </Store.Container>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Store.Container>
+//       <App />
+//     </Store.Container>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
 
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+const BLM = () => {
+  window.location.replace('https://blacklivesmatters.carrd.co/');
+  return null;
 };
 
-appHeight();
-window.addEventListener('resize', appHeight);
+ReactDOM.render(<BLM />, document.getElementById('root'));
+
+// const appHeight = () => {
+//   const doc = document.documentElement;
+//   doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+// };
+
+// appHeight();
+// window.addEventListener('resize', appHeight);
 
 serviceWorker.unregister();
