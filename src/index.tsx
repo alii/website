ae48a962cfca28a9657f397c932b8bd69703871e
Alistair Smith <alistairsmith01@gmail.com>
2020-06-23 08:51:16 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-// import Store from './core/store';
+import Store from './core/store';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Store.Container>
-//       <App />
-//     </Store.Container>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Store.Container>
+      <App />
+    </Store.Container>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
-const BLM = () => {
-  window.location.replace('https://blacklivesmatters.carrd.co/');
-  return null;
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
 };
 
-ReactDOM.render(<BLM />, document.getElementById('root'));
-
-// const appHeight = () => {
-//   const doc = document.documentElement;
-//   doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-// };
-
-// appHeight();
-// window.addEventListener('resize', appHeight);
+appHeight();
+window.addEventListener('resize', appHeight);
 
 serviceWorker.unregister();
