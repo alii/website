93f53755ad39b8dae71c26d96450d0950fb0842c
Alistair Smith <alistairsmith01@gmail.com>
2020-08-27 13:48:30 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { useWindowSize } from '../core/hooks';
 
 const getTime = () => {
   const hour = new Date().getHours();
-  if (hour >= 4 && hour <= 11) return 'morning';
+  if (hour <= 11) return 'morning';
   if (hour >= 12 && hour <= 16) return 'afternoon';
   if (hour >= 17) return 'evening';
 };
