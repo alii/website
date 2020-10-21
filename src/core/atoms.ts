718a6efe8c752b9951fe0cbba92b6ff3afa33e88
Alistair Smith <alistairsmith01@gmail.com>
2020-10-21 23:56:13 +0100

    should not be on by defaul

--- src/core/atoms.ts
+++ src/core/atoms.ts
@@ -3,4 +3,4 @@ import { atom } from 'jotai';
 export const initialBackground = `https://source.unsplash.com/ciO5L8pin8A/${window.innerWidth}x${window.innerHeight}`;
 export const background = atom<string>(initialBackground);
 
-export const modalOpen = atom<boolean>(true);
+export const modalOpen = atom<boolean>(false);
