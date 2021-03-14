04dc55652d078bed65199a9d857f2ebb2f68e567
Alistair Smith <alistairsmith01@gmail.com>
2021-03-14 01:45:52 +0000

    set appheight on doc

--- public/height.js
+++ public/height.js
@@ -1,7 +1,5 @@
-const root = document.getElementById('app');
-
 const listener = () => {
-  root.style.setProperty('--app-height', `${window.innerHeight}px`);
+  document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
 };
 
 window.addEventListener('resize', listener);
