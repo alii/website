7151d35d556fba05a0089391d7840cf094ce1631
sid <hi@sid.fyi>
2021-05-09 02:05:29 -0700

    Improvements (#18)
    - Upgrade to Next.js 10.2
    - Use Webpack 5, React Strict Mode, and Turbo Mode (next.config.js)
    - Use next/image for the background
    - Removed unused "toBackground" function

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    turboMode: true,
+  },
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true,
+  },
+  images: {
+    domains: ["source.unsplash.com"],
+  },
+};
