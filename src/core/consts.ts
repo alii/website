06c6f517efe9ca098f166de9f02ef17b95b411a0
Alistair Smith <alistairsmith01@gmail.com>
2021-08-27 21:50:09 +0100

    Update consts.ts

--- src/core/consts.ts
+++ src/core/consts.ts
@@ -3,5 +3,5 @@ export const Consts = {
 	LastFMToken: 'b6ad96319cd457234c3fc87a03bb0989',
 	DiscordUsername: 'alistair#9999',
 	DiscordId: '268798547439255572',
-	InitialBackground: 'https://source.unsplash.com/in9-n0JwgZ0/1920x1080',
+	InitialBackground: 'https://source.unsplash.com/ay53qag90W8/1920x1080',
 } as const;
