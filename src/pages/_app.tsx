f98897d96c221b3c2a6694bc7c492dccc5d75c56
Alistair Smith <alistairsmith01@gmail.com>
2021-09-06 16:39:37 +0100

    ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,7 +39,21 @@ export default function App({Component, pageProps, router}: AppProps) {
 			<SWRConfig
 				value={{
 					async fetcher(url: string) {
-						return fetch(url).then(async res => res.json());
+						const request = await fetch(url);
+						const json = (await request.json()) as unknown;
+
+						if (request.status >= 400) {
+							let message: string;
+
+							if (json && typeof json === 'object' && 'message' in json) {
+								// Safe to assert because of the ??= underneath this
+								message = (json as {message?: string}).message!;
+							}
+
+							message ??= 'Something went wrong';
+
+							throw new Error(message);
+						}
 					},
 				}}
 			>
