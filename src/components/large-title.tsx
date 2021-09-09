71b458355e7bebc4fd3d8eafe8ced4f816ff2125
Alistair Smith <alistairsmith01@gmail.com>
2021-09-09 18:39:41 +0100

    fix

--- src/components/large-title.tsx
+++
@@ -1,5 +0,0 @@
-import React, {ReactNode} from 'react';
-
-export function LargeTitle({children}: {children: ReactNode}) {
-	return <h1 className="title text-6xl font-bold text-center">{children}</h1>;
-}
