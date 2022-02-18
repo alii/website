0beff32b20aa382bf2fb19fdabcbc56129233705
Alistair Smith <hi@alistair.sh>
2022-02-18 16:36:49 +0100

    feat: add alistair/id

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,10 +1,13 @@
 import {api} from '../../server/api';
 
+import {id} from 'alistair/id';
+
 export default api({
 	async GET() {
 		return {
 			ping: 'pong',
 			time: Date.now(),
+			id: id(),
 		};
 	},
 });
