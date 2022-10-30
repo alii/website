289b38a27e92b2b9dec50fd3538eb5b6b416b317
Alistair Smith <hi@alistair.sh>
2022-10-30 17:20:51 +0000

    fix: actual age

--- src/util/time.ts
+++ src/util/time.ts
@@ -1,11 +1,5 @@
-const bday = new Date('2 November 2004 00:07:00 GMT');
-const ageMilliseconds = Date.now() - bday.getTime();
+const birth = Date.parse(new Date('2 November 2004 07:00 GMT').toString());
 
-export const MILLISECOND = 1;
-export const SECOND = MILLISECOND * 1000;
-export const MINUTE = SECOND * 60;
-export const HOUR = MINUTE * 60;
-export const DAY = HOUR * 24;
-export const YEAR = DAY * 365;
+const msSinceBirth = Date.parse(Date()) - birth;
 
-export const age = ageMilliseconds / YEAR;
+export const age = msSinceBirth / 1000 / 60 / 60 / 24 / 366;
