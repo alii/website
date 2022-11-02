b81dfc0b9398a2f37c1f4148efa4026d83fcf44b
Alistair Smith <hi@alistair.sh>
2022-11-02 00:09:16 +0000

    chore: im 18

--- src/util/time.ts
+++ src/util/time.ts
@@ -2,4 +2,4 @@ const birth = Date.parse(new Date('2 November 2004 07:00 GMT').toString());
 
 const msSinceBirth = Date.parse(Date()) - birth;
 
-export const age = msSinceBirth / 1000 / 60 / 60 / 24 / 366;
+export const age = msSinceBirth / 1000 / 60 / 60 / 24 / 365;
