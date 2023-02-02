f1a99541fab4d4a27ee80d375124051cfb2742a7
Alistair Smith <hi@alistair.sh>
2023-02-02 15:52:55 +0000

    feat: add lint

--- src/hooks/timers.ts
+++ src/hooks/timers.ts
@@ -3,6 +3,13 @@ import {DependencyList, useEffect} from 'react';
 export function useInterval(ms: number, options: {callback: () => void; deps: DependencyList}) {
 	useEffect(() => {
 		const id = setInterval(options.callback, ms);
-		return () => clearInterval(id);
-	}, options.deps);
+
+		return () => {
+			clearInterval(id);
+		};
+
+		// Safe to ignore, because the caller is responsible for passing
+		// valid dependencies.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ms, ...options.deps]);
 }
