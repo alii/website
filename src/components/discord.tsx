e5057e2f49e0cb36ae27f75d7f807b686e17be45
Alistair Smith <hi@alistair.sh>
2023-02-02 07:13:51 +0000

    feat: improve discord box

--- src/components/discord.tsx
+++ src/components/discord.tsx
@@ -1,34 +1,30 @@
 'use client';
 
 import clsx from 'clsx';
-import Link from 'next/link';
 import {SiDiscord} from 'react-icons/si';
-import {hoverClassName} from './card';
 import {Props} from './spotify';
 import {useUpdatingLanyard} from '../hooks/lanyard';
 
 export function Discord(props: Props) {
 	const {data: lanyard} = useUpdatingLanyard(props.lanyard.discord_user.id, props.lanyard);
 
+	const status = lanyard?.discord_status ?? 'offline';
+
 	return (
-		<Link
-			href="https://twitter.com/alistaiir"
-			target="_blank"
-			rel="noopener noreferrer"
+		<div
 			className={clsx(
-				'group col-span-2 flex h-52 items-center justify-center rounded-2xl text-4xl',
+				'group col-span-2 flex h-52 items-center justify-center rounded-2xl text-4xl transition',
 				{
-					online: 'bg-green-500 text-white hover:bg-green-600',
-					idle: 'bg-orange-400 text-white hover:bg-orange-500',
-					dnd: 'bg-red-500 text-white hover:bg-red-600',
-					offline: 'hover:bg-blurple-600 bg-blurple text-white',
-				}[lanyard?.discord_status ?? 'offline'],
-				hoverClassName,
+					online: 'bg-green-500 text-white',
+					idle: 'bg-orange-400 text-white ',
+					dnd: 'bg-red-500 text-white ',
+					offline: 'bg-blurple text-white',
+				}[status],
 			)}
 		>
-			<span className="transform-gpu transition group-hover:-rotate-[10deg] group-hover:scale-[1.3]">
-				<SiDiscord />
-			</span>
-		</Link>
+			<p className="flex -rotate-[8deg] scale-[1.2] items-center space-x-4 font-bold">
+				<SiDiscord /> <span>{status}</span>
+			</p>
+		</div>
 	);
 }
