35650dd4da9ec3cf3eaede35adf862ddece9d443
Alistair Smith <hi@alistair.sh>
2023-02-02 18:09:15 +0000

    fix

--- src/components/card.ts
+++ src/components/card.ts
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 
 export const hoverClassName = clsx(
-	'transform-gpu transition duration-500 will-change-transform hover:scale-95 active:scale-100',
-	'border-4 border-transparent hover:border-white/75 dark:hover:white/10',
+	'transform-gpu transition-all duration-500 will-change-[border,_transform] hover:scale-95 active:scale-100',
+	'border-0 hover:border-4 border-transparent hover:border-white/75 dark:hover:white/10',
 );
