dd88cf832ad2fb2a70d45a9f955df5027a96bfbe
Alistair Smith <hi@alistair.sh>
2023-02-28 15:46:33 +0000

    feat: add redirects back

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ import {config as dotenv} from 'dotenv';
 
 /** @type {import("next").NextConfig} */
 const config = {
+	env: dotenv(),
+
 	images: {
 		remotePatterns: [
 			{
@@ -19,7 +21,36 @@ const config = {
 			},
 		],
 	},
-	env: dotenv(),
+
+	async redirects() {
+		return [
+			{
+				source: '/outro',
+				destination: 'https://www.youtube.com/watch?v=HeF11Av9WuU',
+				permanent: true,
+			},
+			{
+				source: '/desu',
+				destination: 'https://www.youtube.com/watch?v=HotGxCSas6A',
+				permanent: true,
+			},
+			{
+				source: '/10',
+				destination: 'https://youtu.be/G5HcvgepK-I',
+				permanent: true,
+			},
+			{
+				source: '/lulzsec',
+				destination: 'https://www.youtube.com/watch?v=DurOYPdXyF4',
+				permanent: true,
+			},
+			{
+				source: '/wheels',
+				destination: 'https://www.youtube.com/watch?v=9xRFN2i1cwQ',
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default config;
