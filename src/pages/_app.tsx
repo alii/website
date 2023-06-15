76506a60977b5c7ac5bdbd9a064b02b8e3441233
Alistair Smith <hi@alistair.sh>
2023-06-16 00:05:46 +0100

    remove the smooth scrolling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../globals.css';
 
-import Lenis from '@studio-freight/lenis';
 import type {AppProps} from 'next/app';
 import {Nanum_Myeongjo} from 'next/font/google';
 import font from 'next/font/local';
 import Head from 'next/head';
-import {useEffect} from 'react';
 import {Toaster} from 'react-hot-toast';
 
 const title = Nanum_Myeongjo({
@@ -19,35 +17,6 @@ const body = font({
 });
 
 export default function App({Component, pageProps}: AppProps) {
-	useEffect(() => {
-		if (typeof window === 'undefined') {
-			return;
-		}
-
-		const lenis = new Lenis({
-			smoothWheel: true,
-		});
-
-		let rendering = true;
-
-		const render = (time: number) => {
-			if (!rendering) {
-				return;
-			}
-
-			lenis.raf(time);
-
-			requestAnimationFrame(render);
-		};
-
-		requestAnimationFrame(render);
-
-		return () => {
-			lenis.destroy();
-			rendering = false;
-		};
-	}, []);
-
 	return (
 		<>
 			<style jsx global>
