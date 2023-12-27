7b6ab6138fe8e1f3a6e3f2bd33902bd45f7a41f1
Alistair Smith <hi@alistair.sh>
2023-12-27 00:15:45 +0000

    fix appurl env

--- src/server/env.ts
+++ src/server/env.ts
@@ -8,30 +8,12 @@ export const env = z
 		APPLE_PRIV_KEY: z.string(),
 		DISCORD_WEBHOOK: z.string().url(),
 		TURNSTILE_SECRET_KEY: z.string(),
-		VERCEL_URL: z
-			.string()
-			.default('localhost:3000')
-			.transform(value => {
-				if (value === 'localhost:3000') {
-					return 'http://localhost:3000';
-				}
-
-				return `https://${value}`;
-			})
-			.pipe(z.string().url()),
+		APP_URL: z.string().default('http://localhost:3000').pipe(z.string().url()),
 		DEFAULT_LOCATION: z.string().default('London'),
 		MONZO_CLIENT_ID: z
 			.string()
 			.refine((id): id is Id<'oauth2client'> => validateId(id, 'oauth2client')),
 		MONZO_CLIENT_SECRET: z.string(),
 		JWT_SIGNING_SECRET: z.string(),
-	})
-	.transform(result => {
-		const {VERCEL_URL, ...rest} = result;
-
-		return {
-			APP_URL: VERCEL_URL,
-			...rest,
-		};
 	})
 	.parse(process.env);
