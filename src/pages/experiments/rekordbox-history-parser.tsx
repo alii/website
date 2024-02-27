86b06da686a7d6d5c9af50ca6cc00e61d26e0a5b
Alistair Smith <hi@alistair.sh>
2024-02-27 00:17:58 +0000

    freaking rekordbox man 😐

---
+++ src/pages/experiments/rekordbox-history-parser.tsx
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+
+export default function RekordboxHistoryParser() {
+	const [state, setState] = useState('');
+
+	const result = state
+		.split('\n')
+		.map(l => l.split('\t'))
+		.slice(1)
+		.map(l => {
+			const [, title, artist, album, bpm, time, key] = l as [
+				string,
+				string,
+				string,
+				string,
+				string,
+				string,
+				string,
+			];
+
+			return {
+				title,
+				artist,
+				album,
+				bpm,
+				time,
+				key,
+			};
+		})
+		.map(l => `${l.title} - ${l.artist}`)
+		.join('\n');
+
+	return (
+		<div className="flex space-x-4">
+			<textarea value={state} className="bg-neutral-800" onChange={e => setState(e.target.value)} />
+
+			<pre>{result}</pre>
+		</div>
+	);
+}
