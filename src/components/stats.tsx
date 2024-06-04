2f8073538867ca3d9bdfa3d832c527ead633651e
Alistair Smith <hi@alistair.sh>
2024-06-04 22:46:40 +0100

    ok

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -8,7 +8,7 @@ export function Stats() {
 	const firstEverLoadTime = useMemo(() => new Date(stats.time), [stats.time]);
 
 	return (
-		<div className="m-4 mx-auto max-w-2xl rounded-md bg-blue-900 p-4 py-12">
+		<div className="m-4 mx-auto max-w-2xl rounded-md bg-blue-100 p-6 py-12 text-blue-800 dark:bg-blue-900 dark:text-blue-100">
 			<p>
 				You first visited my website on {firstEverLoadTime.toLocaleDateString()} at{' '}
 				{firstEverLoadTime.toLocaleTimeString()} and on this first visit, you were on the{' '}
