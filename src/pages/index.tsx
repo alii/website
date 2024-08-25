2b81dac8e1fdbc3d8cd1a4f6450e38a7954d2140
colemayke <cole.mayke@outlook.com>
2024-08-25 20:04:03 +0900

    fixed typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -115,7 +115,7 @@ export default function Home(props: Props) {
 									>
 										gambit technologies
 									</Link>
-									. we create lifelike ai to captivate customers for businesses. 🤖
+									. we create lifelike ai to captivate a business'customers. 🤖
 								</>
 							),
 						},
