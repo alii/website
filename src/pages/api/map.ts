c0d1a6d3db2cb625b47d64d0fd241e899da44287
colemayke <cole.mayke@outlook.com>
2024-08-25 18:20:43 +0900

    removed applemaps

--- src/pages/api/map.ts
+++ src/pages/api/map.ts
@@ -1,26 +1,16 @@
 import {NextkitError} from 'nextkit';
-import {z} from 'zod';
 import {api} from '../../server/api';
-import {getMapURL} from '../../server/apple-maps';
-
-const querySchema = z.object({
-	theme: z.union([z.literal('light'), z.literal('dark')]),
-});
 
 export default api({
-	GET: async ({ctx, req}) => {
-		const lanyard = await ctx.lanyard.get();
+    GET: async ({ctx}) => {
+        const lanyard = await ctx.lanyard.get();
 
-		if (!lanyard.kv.location) {
-			throw new NextkitError(404, 'No location found');
-		}
+        if (!lanyard.kv.location) {
+            throw new NextkitError(404, 'No location found');
+        }
 
-		const {theme} = querySchema.parse(req.query);
-
-		const mapURL = getMapURL(lanyard.kv.location, theme);
-
-		return new Response(await fetch(mapURL).then(res => res.arrayBuffer()), {
-			headers: {'Content-Type': 'image/png'},
-		});
-	},
+        return {
+            location: lanyard.kv.location
+        };
+    },
 });
