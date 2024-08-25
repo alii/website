c7e48ea85f1191906638959760595abd1f7aadf4
colemayke <cole.mayke@outlook.com>
2024-08-25 18:09:53 +0900

    stuff

--- src/server/env.ts
+++ src/server/env.ts
@@ -4,12 +4,6 @@ export const env = z
     .object({
         APP_URL: z.string().default('http://localhost:3000').pipe(z.string().url()),
         JWT_SIGNING_SECRET: z.string(),
-        // DISCORD_WEBHOOK: z.string().url(),
-        // TURNSTILE_SECRET_KEY: z.string(),
         DEFAULT_LOCATION: z.string().default('Toronto'),
-        // MONZO_CLIENT_ID: z
-        //     .string()
-        //     .refine((id): id is Id<'oauth2client'> => validateId(id, 'oauth2client')),
-        // MONZO_CLIENT_SECRET: z.string(),
     })
     .parse(process.env);
