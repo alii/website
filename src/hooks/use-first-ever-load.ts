f48436858fdac48d06d7588c0f35bbbadf616141
Alistair Smith <hi@alistair.sh>
2025-05-18 19:08:12 -0700

    tidy

--- src/hooks/use-first-ever-load.ts
+++ src/hooks/use-first-ever-load.ts
@@ -4,17 +4,13 @@ import {useRouter} from 'next/router';
 export function useFirstEverLoad() {
 	const router = useRouter();
 
-	return useLocalStorage('user:first-ever-load', () => {
-		return {
-			path: router.pathname,
-			time: Date.now(),
-			query: router.query,
-		};
-	});
+	return useLocalStorage('user:first-ever-load', () => ({
+		path: router.pathname,
+		time: Date.now(),
+		query: router.query,
+	}));
 }
 
 export function useVisitCounts() {
-	return useLocalStorage('user:visit-counts', () => {
-		return 1;
-	});
+	return useLocalStorage('user:visit-counts', () => 1);
 }
