3645047fd114627090e973e5b620fa643fd1da77
Alistair Smith <hi@alistair.sh>
2025-09-19 18:50:09 -0700

    more changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import {Link} from 'bun-framework-react/link';
+
 import {useLanyardWS} from 'use-lanyard';
 import {discordId} from '../src/utils/constants';
 
@@ -8,6 +10,7 @@ export default function Home() {
 
 	return (
 		<main className="mx-auto max-w-xl px-3 pt-24 pb-16">
+			<Link href="/about">About</Link>
 			<pre>{JSON.stringify(lanyard, null, 2)}</pre>
 		</main>
 	);
