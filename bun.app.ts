13de27b6621931ee55812516ea21ef9bfe6e4326
Alistair Smith <hi@alistair.sh>
2025-10-03 17:50:01 -0700

    framework

--- bun.app.ts
+++ bun.app.ts
@@ -1,25 +1,5 @@
 import react from 'bun-framework-react';
 
-const bundlerOptions = {
-	minify: {
-		whitespace: false,
-		syntax: false,
-		identifiers: false,
-	},
-};
 export default {
-	app: {
-		framework: {
-			...react,
-			bundlerOptions: {
-				...bundlerOptions,
-				...react.bundlerOptions,
-			},
-		},
-		bundlerOptions: {
-			server: bundlerOptions,
-			client: bundlerOptions,
-			ssr: bundlerOptions,
-		},
-	},
+	app: {framework: react},
 };
